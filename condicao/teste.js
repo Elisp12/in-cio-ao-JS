106e4a6b8ade58fb18ad7bd24f743e6a75a0bc58
Elisp12 <eliseuspdam.1999@gmail.com>
2024-11-27 14:10:44 -0300

    condição simples e composta if else

---
+++ condicao/teste.js
@@ -0,0 +1,16 @@
+// condicao simples
+let numero = 5
+let idade = 18
+
+if (numero > 2) {
+    console.log('é maior') 
+}else {
+    console.log('é menor')
+}
+
+// condição composta 
+if (numero == 5 && idade >= 18){
+    console.log('Maior de idade')
+}else{
+    console.log('menor de idade')
+}
